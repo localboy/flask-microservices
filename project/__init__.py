72db0c81af0728a324e16192249b6f1c70d55d4b
mohammadjh <mjakir.ddd@gmail.com>
2017-08-16 01:40:12 +0600

    Added envirenment variable in docker-compose.yml

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,5 @@
 # project/__init__.py
-
+import os
 from flask import Flask, jsonify
 
 
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 
 # set app config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings=os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 @app.route('/ping', methods=['GET'])
